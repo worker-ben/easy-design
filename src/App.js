65947b748054efb45789b6411f372f57c1078ed6
weizhibin <weizhibin.66@bytedance.com>
2020-12-22 15:48:31 +0800

    feat: 改了一下目录结构
    Change-Id: Idb590276b170918218e6f9a8809483325d8e64b7

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
 import ClickButton from './components/Click';
 
+/**
+ * 用来测试自己实现的组件
+ */
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <Hello />
         <ClickButton />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
