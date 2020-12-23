40dcfd0ac0862c338355725060dcfdd6acfbd706
weizhibin <weizhibin.66@bytedance.com>
2020-12-23 17:05:38 +0800

    feat: 添加大小样式
    Change-Id: I8ff06155f76f2019ae984b1ff25d1c87fdc6b795

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App">
       {/* 支持原生属性了 */}
-      <Button onClick={()=>{console.log('Default Button')}} > Default Button </Button>
-      <Button onClick={()=>{console.log('Primary Button')}} btnType={'primary'}> Primary Button </Button>
-      <Button onClick={()=>{console.log('Danger Button')}} btnType={'danger'}> Danger Button </Button>
-      <Button  btnType={'link'} href={'https://www.baidu.com/'} target='_blank' > Hello Link </Button>
+      <Button onClick={() => { console.log('Default Button') }} > Default Button </Button>
+      <Button onClick={() => { console.log('Primary Button') }} size={'sm'} btnType={'primary'}> Primary Button </Button>
+      <Button onClick={() => { console.log('Danger Button') }} size={'lg'} btnType={'danger'}> Danger Button </Button>
+      <Button btnType={'link'} href={'https://www.baidu.com/'} target='_blank' > Hello Link </Button>
       <Button disabled > Disabled Button </Button>
     </div>
   );
